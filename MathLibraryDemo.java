09efa03dac5c576d5d745418eae6d822b02f4f1f
Milky621983 <cuadranorylenne_bsit@plmun.edu.ph>
2025-02-10 15:45:26 +0700

    MathLibraryDemo.java

---
+++ MathLibraryDemo.java
@@ -0,0 +1,21 @@
+public class MathLibraryExample {
+    public static void main(String[] args) {
+        // Use Math.pow() to calculate power
+        double power = Math.pow(2, 3);  // 2 raised to the power of 3
+
+        // Use Math.sqrt() to calculate the square root
+        double squareRoot = Math.sqrt(16);  // Square root of 16
+
+        // Use Math.round() to round a number
+        long roundedNumber = Math.round(5.6);  // Rounding 5.6
+
+        // Use Math.random() to generate a random number
+        double randomNumber = Math.random();  // Random number between 0.0 and 1.0
+
+        // Output the results
+        System.out.println("Power: " + power);
+        System.out.println("Square Root: " + squareRoot);
+        System.out.println("Rounded Number: " + roundedNumber);
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
